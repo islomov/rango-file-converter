7e0e4e76c5371d91f5937c9f9f29ec89f1f6db49
Sardor <sardor.islomov.96@gmail.com>
2026-02-20 16:50:17 +0500

    Initial Commit

---
+++ rangofileconverter/rangofileconverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  rangofileconverterApp.swift
+//  rangofileconverter
+//
+//  Created by Sardor Islomov on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct rangofileconverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
